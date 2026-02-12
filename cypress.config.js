b151fd38348897548f1b71a7b2e63a9dd683e7f6
seva9523 <seva9523@gmail.com>
2026-02-12 16:52:28 +0000

    Create cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/**/*.cy.js",
+    baseUrl: "https://porfolio-visualizer-taca.vercel.app",
+  },
+});
