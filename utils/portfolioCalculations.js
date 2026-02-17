d12571c3a1519ef482084af56dac4e423a1d9d26
seva9523 <seva9523@gmail.com>
2026-02-17 11:20:58 +0000

    Update portfolioCalculations.js

--- utils/portfolioCalculations.js
+++ utils/portfolioCalculations.js
@@ -1,26 +1,21 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
-diff --git a/utils/portfolioCalculations.js b/utils/portfolioCalculations.js
-index 5f2fb765072a7cb05f0aeb80f4448a52b73d7587..14fd8659a1b0c41f787fd2ac5f43d19f50d94ac7 100644
---- a/utils/portfolioCalculations.js
-+++ b/utils/portfolioCalculations.js
-@@ -1,12 +1,17 @@
- // Example helper functions your tests will call
- 
- export function calculateExpectedReturn(portfolio) {
-   return portfolio.reduce((acc, asset) => acc + asset.weight * asset.expectedReturn, 0);
- }
- 
- export function calculateFutureValue(currentSavings, monthlyContribution, annualReturn, years) {
-   const r = annualReturn / 12;
-   const n = years * 12;
-+
-+  if (r === 0) {
-+    return currentSavings + monthlyContribution * n;
-+  }
-+
-   return currentSavings * Math.pow(1 + r, n) +
-          monthlyContribution * ((Math.pow(1 + r, n) - 1) / r);
- }
- 
-EOF
-)
+// utils/portfolioCalculations.js
+function calculateExpectedReturn(portfolio) {
+  return portfolio.reduce((acc, asset) => acc + asset.weight * asset.expectedReturn, 0);
+}
+
+function calculateFutureValue(currentSavings, monthlyContribution, annualReturn, years) {
+  const r = annualReturn / 12;
+  const n = years * 12;
+
+  if (r === 0) return currentSavings + monthlyContribution * n;
+
+  return (
+    currentSavings * Math.pow(1 + r, n) +
+    monthlyContribution * ((Math.pow(1 + r, n) - 1) / r)
+  );
+}
+
+module.exports = {
+  calculateExpectedReturn,
+  calculateFutureValue,
+};
