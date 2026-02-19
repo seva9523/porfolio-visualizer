a10991f89303fae2129c5bb47953205be4dc244b
seva9523 <seva9523@gmail.com>
2026-02-19 12:54:17 +0000

    Update themes_basic.cy.js

--- cypress/e2e/themes/themes_basic.cy.js
+++ cypress/e2e/themes/themes_basic.cy.js
@@ -7,6 +7,18 @@ describe("Theme Explorer - Basic", () => {
     cy.get('#load-bar', { timeout: 25000 }).should('not.be.visible');
     cy.get('[data-testid="theme-card"]', { timeout: 20000 })
       .should('have.length.greaterThan', 10);
+    
+    // Dismiss onboarding modal if present
+    cy.get('body').then(($body) => {
+      if ($body.find('[data-testid="onboarding-modal"]').length > 0) {
+        cy.get('[data-testid="onboarding-modal"]').then(($modal) => {
+          if ($modal.is(':visible')) {
+            cy.get('[data-testid="onboarding-modal"]').find('button').first().click({ force: true });
+          }
+        });
+      }
+    });
+    
     cy.wait(2000);
   }
 
