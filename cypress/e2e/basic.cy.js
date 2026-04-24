f83d60f02d079773c516d501a24b72d2a0c7f43e
seva9523 <seva9523@gmail.com>
2026-04-24 13:02:39 +0100

    Delete cypress directory

--- cypress/e2e/basic.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Homepage loads', () => {
-  it('Visits the homepage successfully', () => {
-    cy.visit('https://porfolio-visualizer-taca.vercel.app/');
-    cy.contains('Portfolio').should('exist');
-  });
-});
